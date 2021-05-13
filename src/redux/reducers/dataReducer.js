9ae4817303ae249f9a27774a6535cfd48a2b061d
Rusty Hoppins <rustiphyde@gmail.com>
2021-05-13 17:34:07 -0500

    Create dataReducer file and slydeshow fetch all case

---
+++ src/redux/reducers/dataReducer.js
@@ -0,0 +1,27 @@
+import {
+	SET_SLYDESHOWS
+} from "../types";
+
+const initialState = {
+	slydeshows: [],
+	
+};
+
+export default function (state = initialState, action) {
+	switch (action.type) {
+		case LOADING_DATA:
+			return {
+				...state,
+				loading: true,
+			};
+		case SET_SLYDESHOWS:
+			return {
+				...state,
+				slydeshows: action.payload,
+				loading: false,
+			};
+		
+		default:
+			return state;
+	}
+}
