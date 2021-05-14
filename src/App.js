f0836ee747e65d916654fe16965f112594a432ed
Rusty Hoppins <rustiphyde@gmail.com>
2021-05-14 15:13:29 -0500

    Add necessary imports and baseUrl default

--- src/App.js
+++ src/App.js
@@ -1,6 +1,40 @@
+import React, { Component } from 'react';
 import './App.css';
-import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import jwtDecode from 'jwt-decode';
+import axios from 'axios';
+
+// Firebase
+import firebase from 'firebase/app';
+import { config } from './util/config';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { SET_AUTHENTICATED } from './redux/types';
+import { createFirestoreInstance } from 'redux-firestore';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+
+
+// Util
+import themeFile from './util/theme';
+import AuthRoute from './util/AuthRoute';
+
+// MUI Components
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+// Components
+
+// Pages
+
 import SlydroLogo from './icons/SlydroLogo';
+
+axios.defaults.baseURL = "https://us-central1-slydro-2327.cloudfunctions.net/api";
+
+const theme = createMuiTheme(themeFile);
 
 function App() {
   return (
