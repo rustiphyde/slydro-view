90658fa9b0d7f388bad2190e401e5f7b6ccd9f65
Rusty Hoppins <rustiphyde@gmail.com>
2021-05-14 15:10:13 -0500

    Add theme file just in case

---
+++ src/util/theme.js
@@ -0,0 +1,44 @@
+const theme = {
+
+    typography: {
+      useNextVariants: true
+    },
+    form: {
+      textAlign: "center"
+    },
+    image: {
+      margin: "0 auto"
+    },
+    pageTitle: {
+      margin: "0 auto 10px auto"
+    },
+    displayLinebreaks: {
+      whiteSpace: "pre-wrap"
+    },
+    textField: {
+      margin: "10px auto 10px auto",
+      textAlign: "center",
+      padding: "0 16px !important"
+    },
+    button: {
+      margin: "20px auto",
+      width: "200px",
+      position: "relative"
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: "10px"
+    },
+    customMessage: {
+      color: "#ff9800",
+      fontSize: "1rem",
+      marginTop: "10px"
+    },
+    progress: {
+      position: "absolute"
+    }
+//   }
+};
+
+export default theme;
