dfc91634dcf7441abbabdceac91c5ba681bd50d2
Rusty Hoppins <rustiphyde@gmail.com>
2021-07-07 19:02:52 -0500

    Format Code

--- src/util/Droppable.js
+++ src/util/Droppable.js
@@ -1,55 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import * as dropEffects from './dropEffects';
+import * as dropEffects from "./dropEffects";
 
 const insideStyle = {
-    opacity: .5,
+	opacity: 0.5,
 };
 
-const Droppable = props => {
-    const [isOver, setIsOver] = useState(false);
+const Droppable = (props) => {
+	const [isOver, setIsOver] = useState(false);
 
-    const dragOver = e => {
-        e.preventDefault();
-        e.dataTransfer.dropEffect = props.dropEffect;
-    };
+	const dragOver = (e) => {
+		e.preventDefault();
+		e.dataTransfer.dropEffect = props.dropEffect;
+	};
 
-    const drop = ev => {
-        const droppedItem = ev.dataTransfer.getData("drag-item");
-        if (droppedItem) {
-            props.onItemDropped(droppedItem);
-        }
-        setIsOver(false);
-    };
+	const drop = (ev) => {
+		const droppedItem = ev.dataTransfer.getData("drag-item");
+		if (droppedItem) {
+			props.onItemDropped(droppedItem);
+		}
+		setIsOver(false);
+	};
 
-    const dragEnter = ev => {
-        ev.dataTransfer.dropEffect = props.dropEffect;
-        setIsOver(true);
-    };
+	const dragEnter = (ev) => {
+		ev.dataTransfer.dropEffect = props.dropEffect;
+		setIsOver(true);
+	};
 
-    const dragLeave = () => setIsOver(false);
+	const dragLeave = () => setIsOver(false);
 
-    return (
-        <div
-            onDragOver={dragOver}
-            onDrop={drop}
-            onDragEnter={dragEnter}
-            onDragLeave={dragLeave}
-            style={{ width: "100%", height: "100%", ...(isOver ? insideStyle : {}) }}
-        >
-            {props.children}
-        </div>
-    );
+	return (
+		<div
+			onDragOver={dragOver}
+			onDrop={drop}
+			onDragEnter={dragEnter}
+			onDragLeave={dragLeave}
+			style={{ width: "100%", height: "100%", ...(isOver ? insideStyle : {}) }}
+		>
+			{props.children}
+		</div>
+	);
 };
 
 Droppable.propTypes = {
-    onItemDropped: PropTypes.func.isRequired,
-    dropEffect: PropTypes.string,
-    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+	onItemDropped: PropTypes.func.isRequired,
+	dropEffect: PropTypes.string,
+	children: PropTypes.oneOfType([
+		PropTypes.arrayOf(PropTypes.node),
+		PropTypes.node,
+	]).isRequired,
 };
 
 Droppable.defaultProps = {
-    dropEffect: dropEffects.All,
+	dropEffect: dropEffects.All,
 };
 
 export default Droppable;
