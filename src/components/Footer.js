7e37059a68c4a1a3503d2e8573dd00ba503ada61
Rusty Hoppins <rustiphyde@gmail.com>
2021-07-07 19:10:34 -0500

    Update look and styles of the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,47 @@
-import React, { Fragment, Component } from 'react';
-import { connect } from 'react-redux';
-import LogoutIcon from '../icons/LogoutIcon';
-import SlydroButton from '../util/SlydroButton';
-import { logoutSlyder } from '../redux/actions/slyderActions';
+import React, { Fragment, Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import LoginIcon from "../icons/LoginIcon";
+import LogoutIcon from "../icons/LogoutIcon";
+import SlydroButton from "../util/SlydroButton";
+import { logoutSlyder } from "../redux/actions/slyderActions";
 
 class Footer extends Component {
+	handleLogout = () => {
+		this.props.logoutSlyder();
+		window.location.href = "/login";
+	};
 
-    handleLogout = () => {
-        this.props.logoutSlyder();
-        window.location.href = "/login";
-    }
+	render() {
+		const { authenticated } = this.props;
 
-    render() {
+		let footerMarkup = authenticated ? (
+			<div>
+				<div className="footer-icons">
+					<SlydroButton
+						onClick={this.handleLogout}
+						tip="LOGOUT"
+						className="logout"
+					>
+						<LogoutIcon className="logout" />
+					</SlydroButton>
+				</div>
+			</div>
+		) : (
+			<div className="footer-icons">
+				<Link to="/login" className="logout">
+					<SlydroButton tip="LOGIN">
+						<LoginIcon className="logout"/>
+					</SlydroButton>
+				</Link>
+			</div>
+		);
+		return <Fragment>{footerMarkup}</Fragment>;
+	}
+}
 
-        const { authenticated } = this.props;
-
-        let footerMarkup = authenticated ? (
-            
-                <div>
-                <div className="footer-icons">
-                    <SlydroButton onClick={this.handleLogout} tip="LOGOUT" className="logout">
-                    <LogoutIcon className="logout"/>
-
-                    </SlydroButton>
-                    
-                    
-
-                </div>
-                </div>
-                
-        ) : (
-            <div  className="footer-icons">
-                <h3 className="title">Logged Out</h3>
-            </div>
-        )
-        return(
-            <Fragment>
-                {footerMarkup}
-            </Fragment>
-            
-        )
-    }
-    }
-
-    const mapStateToProps = (state) => ({
-        authenticated: state.slyder.authenticated
-    })
+const mapStateToProps = (state) => ({
+	authenticated: state.slyder.authenticated,
+});
 
 export default connect(mapStateToProps, { logoutSlyder })(Footer);
