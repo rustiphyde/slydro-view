c697a5096da564a9f829e8b4de3f1b3ecd200ce4
Rusty Hoppins <rustiphyde@gmail.com>
2021-08-11 14:51:37 -0500

    Create basic skeleton of home page

---
+++ src/pages/home.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+
+class home extends Component {
+    render(){
+        return(
+            <Grid container className="centered">
+                
+                <Grid item sm={5}>
+                
+               
+            </Grid>
+            <Grid item sm={2}></Grid>
+            <Grid item sm={5}>
+                 
+            </Grid>
+            
+            </Grid>
+            
+        )
+    }
+}
+
+export default home;
